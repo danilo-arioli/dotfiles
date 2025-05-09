5a684ae85a2b27f433ec09a53e5a0906f27ce317
Danilo <arioli.danilo2000@gmail.com>
2025-05-09 11:12:16 +0200

    fix: remove python related formatters and linters from mason ensure_installed

--- .config/nvim/lua/plugins/lsp/mason.lua
+++ .config/nvim/lua/plugins/lsp/mason.lua
@@ -48,9 +48,6 @@ return {
       ensure_installed = {
         "prettier", -- prettier formatter
         "stylua", -- lua formatter
-        "isort", -- python formatter
-        "black", -- python formatter
-        "pylint",
         "eslint_d",
         "eslint",
         "php-cs-fixer",
